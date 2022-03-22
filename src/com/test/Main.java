1daff5ff51b152268ab7618c2cfe9bdf5b4f0036
Ashok <ashok@vserv.com>
2022-03-22 17:38:58 +0530

    Edited Welcome Message

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -3,6 +3,6 @@ package com.test;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java!!");
     }
 }
